8c00b9afec7ece55f8813e3e7fe321049ba106f1
KJB <drv98@naver.com>
2026-07-03 15:10:46 +0900

    같은 제품 담을때 장바구니의 수량만 증가

--- src/store/useCartStore.js
+++ src/store/useCartStore.js
@@ -3,9 +3,23 @@ import { create } from "zustand";
 const useCartStore = create((set) => ({
   cart: [], //장바구니
   addToCart: (product) =>
-    set((state) => ({
-      cart: [...state.cart, product],
-    })),
+    set((state) => {
+      const isExist = state.cart.find((item) => item.id === product.id);
+      if (isExist) {
+        //이미 장바구니에 있는 상품일 경우에 qantity + 1
+        return {
+          cart: state.cart.map((item) =>
+            item.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item,
+          ),
+        };
+      }
+      return {
+        //새 상품일 경우에 수량을 1로 한다
+        cart: [...state.cart, { ...product, quantity: 1 }],
+      };
+    }),
   removeFromCart: (productId) =>
     set((state) => ({
       cart: state.cart.filter((item) => item.id !== productId),
